a766d7625f59e3432b7ed7b1f65e0d5c846eaa7e
Tyler Beck <t.beck1990@gmail.com>
2022-08-17 14:40:41 -0400

    Shared back-end pieces between API and serverless functions

---
+++ models/fb-review.model.ts
@@ -0,0 +1,27 @@
+import {Document, Schema as MongooseSchema} from 'mongoose/lib';
+import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import {ObjectType, Field} from '@nestjs/graphql';
+
+export interface ICategory extends Document {
+  _id: MongooseSchema.Types.ObjectId;
+  createdAt: Date;
+  review: string;
+}
+
+@ObjectType()
+@Schema()
+export class FbReview implements ICategory {
+  @Field(() => String)
+  _id: MongooseSchema.Types.ObjectId;
+
+  @Field(() => Date)
+  @Prop()
+  createdAt: Date;
+
+  @Field(() => String)
+  @Prop()
+  review: string;
+}
+
+export type FbReviewDocument = FbReview & Document;
+export const FbReviewSchema = SchemaFactory.createForClass(FbReview);
